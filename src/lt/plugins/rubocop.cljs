9af8f9c62eca27b7a34e645aae0be0bddfe48601
Sean Caffery <scaffery@zendesk.com>
2014-03-01 15:29:46 +1100

    First fully working version
    Displays cop violations in a gutter title

--- src/lt/plugins/rubocop.cljs
+++ src/lt/plugins/rubocop.cljs
@@ -6,13 +6,20 @@
             [lt.objs.editor.pool :as pool]
             [lt.objs.files :as files]
             [lt.util.load :as load]
+            [lt.util.dom :as dom]
             [lt.objs.proc :as proc])
   (:require-macros [lt.macros :refer [defui behavior background]]))
 
-(def exec (.-exec (js/require "child_process")))
 (def shell (load/node-module "shelljs"))
 
 (def rubocop-path (.which shell "rubocop"))
+
+(defui cop-marker [this cops]
+  [:div.rubocop-gutter-marker
+   {:style (str " color: blue; background: #ffffff; overflow: hidden")
+    :title (clojure.string/join "\n" cops)}
+   [:div.cop-gutter-dot
+    {:style "width: 8px; height: 10px; background: red;"}]])
 
 (defn violations-for-file [json-str]
   (let [parsed (js->clj (JSON/parse json-str))]
@@ -32,16 +39,39 @@
                           (raise this :cop-finished stdout)
                         )))))
 
+(defn clear-gutters []
+  (let [line-nos (range (editor/line-count (editor/->cm-ed (pool/last-active))))
+        ed (editor/->cm-ed (pool/last-active))]
+    (editor/operation ed (fn []
+      (doseq [idx line-nos]
+        (.setGutterMarker ed idx "cop-gutter" ))))))
+
 (behavior ::cop-finished
           :triggers #{:cop-finished}
-          :reaction (fn [this hints]
-                      (.log js/console #js ["zomg doing" (first (offence-line-map (violations-for-file hints)))])
-                      ))
+          :reaction (fn [this cops]
+                      (let [ed (editor/->cm-ed (pool/last-active))
+                            current-gutters (set (js->clj (editor/option (pool/last-active) "gutters")))
+                            gutter-div (dom/$ :div.CodeMirror-gutters (object/->content (pool/last-active)))
+
+                            line-map (offence-line-map (violations-for-file cops))
+                            cops-by-line (group-by :line line-map)
+                            gutter-markers (map (fn [[line cops]]
+                                                  {:line line :mark (cop-marker ed (map #(:message %) cops))}) cops-by-line)]
+                        (editor/operation ed
+                                          (fn []
+                                            (clear-gutters)
+                                            (editor/set-options ed {:gutters (clj->js (conj current-gutters "cop-gutter"))})
+                                            (dom/set-css (dom/$ :div.cop-gutter gutter-div) {"width" "8px"})
+                                            (doall (map-indexed
+                                                    (fn [idx gutter-marker]
+                                                      (.setGutterMarker ed (dec (:line gutter-marker)) "cop-gutter" (:mark gutter-marker)))
+                                                        gutter-markers)))))))
+
 (behavior ::on-save
-                  :triggers #{:save}
-                  :desc "rubocop: Run rubocop on save"
-                  :reaction (fn [this]
-                              (object/raise this ::run-file)))
+          :triggers #{:save}
+          :desc "rubocop: Run rubocop on save"
+          :reaction (fn [this]
+                      (object/raise this ::run-file)))
 
 (behavior ::run-file
           :triggers #{::run-file}
